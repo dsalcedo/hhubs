2c7348d297e77322e63852d7ac88ba7e1953bf42
danieluyo <dsalcedo@outlook.com>
2016-09-20 01:27:34 -0500

    #dev agregar publicacion - cambios en UI

--- app/Http/Controllers/Manager/Blog/PublicacionController.php
+++ app/Http/Controllers/Manager/Blog/PublicacionController.php
@@ -9,8 +9,18 @@ use App\Http\Controllers\Controller;
 
 class PublicacionController extends Controller
 {
+    public function __construct(Request $request)
+    {
+        $this->req = $request;
+    }
+
     public function agregarPublicacion()
     {
         return view('manager.blog.agregarPublicacion');
+    }
+
+    public function createPublicacion()
+    {
+        return response()->json( $this->req->all());
     }
 }
--- resources/views/manager/blog/agregarPublicacion.blade.php
+++ resources/views/manager/blog/agregarPublicacion.blade.php
@@ -15,27 +15,63 @@
 @endsection
 
 @section('body')
-    <div class="col-md-9">
-        <form>
-            <div class="form-group">
-                <label for="titulo">Título de la publicación</label>
-                <input type="text" class="form-control" id="titulo" placeholder="Escribe un título" maxlength="255" autofocus>
+    {!! Form::open(['route'=>'blog.create.publicacion']) !!}
+    <div class="col-md-9" style="margin-bottom: 30px;">
+        <div class="form-group">
+                {!! Form::text('titulo', null, ['class'=>'form-control', 'placeholder'=>'Escribe un título', 'maxlength'=>255,'autofocus']) !!}
             </div>
             <div class="form-group">
-                <div id="summernote">Hello Summernote</div>
+                <textarea id="summernote" name="post">Hello Summernote</textarea>
             </div>
 
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox"> Check me out
-                </label>
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-        </form>
     </div>
     <div class="col-md-3">
-        {!! Form::open(['route'=>'media.upload.blog', 'files' => true, 'class'=>'dropzone', 'id'=>'mediaUpload']) !!}
+        <div class="col-md-12" style="margin-bottom: 30px;">
+            <div class="row">
+                <span class="btn btn-default"  data-toggle="modal" data-target="#myModal">Cargar imagen destacada</span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="titulo">Estado de la publicación</label>
+            <select class="form-control">
+                <option value="guardado">Guardado</option>
+                <option value="publicadi">Publicado</option>
+            </select>
+        </div>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="destacado" value="1"> Marcar como destacado
+            </label>
+        </div>
+        <div class="col-md-12">
+            <div class="row">
+                <button type="submit" class="btn btn-default">
+                    Guardar publicación
+                </button>
+            </div>
+        </div>
+    </div>
         {!! Form::close() !!}
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    {!! Form::open(['route'=>'media.upload.blog', 'files' => true, 'class'=>'dropzone', 'id'=>'mediaUpload']) !!}
+                    {!! Form::close() !!}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary">Terminar</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -69,13 +105,13 @@
                             return item.indexOf(keyword) === 0;
                         }));
                     }
-                },
-                callbacks: {
+                }
+                /*callbacks: {
                     onImageUpload: function(files) {
                         // Evitamos que se puedan pegar imágenes.
                         //null;
                     }
-                }
+                }*/
             });
         });
 
@@ -87,11 +123,13 @@
             maxFilesize: 1, // MB
             addRemoveLinks: true,
             maxFiles: 1,
+            dictDefaultMessage: "Subir imagen destacada",
             removedfile: function(file) {
                 var _ref;
                 return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
             },
             accept: function(file, done) {
+                console.log(file)
                 if (file.name == "justinbieber.jpg") {
                     done("Naha, you don't.");
                 }
